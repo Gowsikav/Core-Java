b2127811bc6934cec7356cf20ffdfd35c37ab358
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-05-01 11:17:55 +0530

    Starting Servlet example

---
+++ tom-jerry/src/com/xworkz/tom/servlet/Jerry.java
@@ -0,0 +1,22 @@
+package com.xworkz.tom.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/dry",loadOnStartup = 1)
+public class Jerry extends GenericServlet {
+
+    public Jerry()
+    {
+        System.out.println("Jerry constructor...");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("service running in jerry...");
+    }
+}
