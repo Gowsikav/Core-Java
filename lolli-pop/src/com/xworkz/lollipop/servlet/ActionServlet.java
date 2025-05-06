327d23973e671a063c9193ab8ab7939ae12781ba
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-05-06 15:13:39 +0530

    Servlet service method example

---
+++ lolli-pop/src/com/xworkz/lollipop/servlet/ActionServlet.java
@@ -0,0 +1,19 @@
+package com.xworkz.lollipop.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/send",loadOnStartup = 1)
+public class ActionServlet extends GenericServlet {
+    public ActionServlet(){
+        System.out.println("ActionServlet created by Tomcat");
+    }
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("Service method in ActionServlet");
+    }
+}
