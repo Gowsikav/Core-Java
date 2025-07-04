2e8d05f078e642c0e68a0980553d6a56ef0d7c7b
Gowsika V <gowsikavenkatachslam0713@gmail.com>
2025-07-04 18:08:05 +0530

    Maven setup done

---
+++ demo/src/main/java/com/xworkz/demo/internal/Sample.java
@@ -0,0 +1,12 @@
+package com.xworkz.demo.internal;
+
+import lombok.Getter;
+
+@Getter
+public class Sample {
+    public Sample()
+    {
+        System.out.println("Sample constructor");
+    }
+    int num=10;
+}
---
+++ demo/src/main/java/com/xworkz/demo/runner/SampleRunner.java
@@ -0,0 +1,10 @@
+package com.xworkz.demo.runner;
+
+import com.xworkz.demo.internal.Sample;
+
+public class SampleRunner {
+    public static void main(String[] args) {
+        Sample sample=new Sample();
+        System.out.println("Number is " +sample.getNum());
+    }
+}
